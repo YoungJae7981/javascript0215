2ed00c9f97fe8999617bbe1348434156c3cedec6
sample <youngjae7981@gmail.com>
2023-02-16 09:15:11 +0900

    CHANGE

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const box = document.querySelectorAll('.alertBox');
+const box = document.querySelectorAll(".alertBox");
 
 function openBox() {
-  box.forEach(function(loot) {
-    loot.style.display = 'block';
-  })
+  box.forEach(function (loot) {
+    loot.style.display = "block";
+  });
 }
 
-const cBtn1 = document.getElementById('c-btn1');
-const cBtn2 = document.getElementById('c-btn2');
-const cBtn3 = document.getElementById('c-btn3');
+const cBtn1 = document.getElementById("c-btn1");
+const cBtn2 = document.getElementById("c-btn2");
+const cBtn3 = document.getElementById("c-btn3");
 
-cBtn1.addEventListener('click', function() {
-  this.parentElement.style.display = 'none';
-})
+cBtn1.addEventListener("click", function () {
+  this.parentElement.style.display = "none";
+});
 
-cBtn2.addEventListener('click', function() {
-  this.parentElement.style.display = 'none';
-})
+cBtn2.addEventListener("click", function () {
+  this.parentElement.style.display = "none";
+});
 
-cBtn3.addEventListener('click', function() {
-  this.parentElement.style.display = 'none';
-})
+cBtn3.addEventListener("click", function () {
+  this.parentElement.style.display = "none";
+});
